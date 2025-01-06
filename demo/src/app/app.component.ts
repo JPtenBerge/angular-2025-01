d2e19b6ae0bbfb15e398c90979381ab3c4b5ca2d
JP <jp@jp.nl>
2025-01-06 15:09:28 +0100

    Template-driven forms

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -2,31 +2,43 @@ import { CommonModule, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EuroPipe } from './pipes/euro.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, EuroPipe],
+  imports: [RouterOutlet, CommonModule, FormsModule, EuroPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'demo';
+  newConnector = { reversable: false } as Connector;
 
-  name = 'Tijmen';
-
-  nu = new Date();
-  bedrag = 123456.78;
-
-  connectors = [
+  connectors: Connector[] = [
     { name: 'USB Type C', reversable: true, type: 'rectangle', photoUrl: 'https://www.kabelshop.nl/image/Goobay_USB_C_naar_USB_C_kabel_%7C_0.5_meter_%7C_USB_3.2_100%25_koper_Power_Delivery_100_W_Zwart_49252_K010214083_m1_big.jpg' },
     { name: 'USB Type B', reversable: false, type: 'rectangle', photoUrl: 'https://www.kantoorspecialist.nl/autoimg/1128438/1024x1024/resize/showimage.jpg' },
     { name: 'USB Type A', reversable: false, type: 'rectangle', photoUrl: 'https://image.allekabels.nl/image/1513-0/usb-a-kabel-lengte-1.8-meter.jpg' },
     { name: 'PS/2', reversable: false, type: 'round', photoUrl: 'https://media.startech.com/cms/products/gallery_large/kyc1mf.main.jpg' },
-  ]
+  ];
 
-  changeName() {
-    setTimeout(() => {
-      this.name += ' Youri na timeout';
-    }, 2000);
+  addConnector() {
+    // how to clone an object
+    // const newqqq = { ...this.newConnector }; // shallow copy
+    // lodash _.cloneDeep()
+    // JSON.parse(JSON.stringify(this.newConnector)) // deep clone
+    this.connectors.push(structuredClone(this.newConnector));
+    this.newConnector = { reversable: false } as Connector;
   }
+
+  // changeName() {
+  //   setTimeout(() => {
+  //     this.name += ' Youri na timeout';
+  //   }, 2000);
+  // }
+}
+
+interface Connector {
+  name: string;
+  reversable: boolean;
+  type: string;
+  photoUrl: string;
 }
