28a5b65b549a45875377939fde18b20532c20bb8
JP <jp@jp.nl>
2025-01-07 14:25:30 +0100

    Backendcommunicatie laten zien

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,48 +1,58 @@
 import { CommonModule, UpperCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EuroPipe } from './pipes/euro.pipe';
 import { FormsModule } from '@angular/forms';
-import { LifeComponent } from "./components/life/life.component";
-import { AutocompleterComponent } from "./components/autocompleter/autocompleter.component";
+import { LifeComponent } from './components/life/life.component';
+import { AutocompleterComponent } from './components/autocompleter/autocompleter.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormsModule, EuroPipe, LifeComponent, AutocompleterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	imports: [RouterOutlet, CommonModule, FormsModule, EuroPipe, LifeComponent, AutocompleterComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
-export class AppComponent {
-  newConnector = { reversable: false } as Connector;
+export class AppComponent implements OnInit {
+	newConnector = { reversable: false } as Connector;
+	showLife = false;
+	connectors?: Connector[];
+	http = inject(HttpClient);
 
-  showLife = false;
+	isFetchingConnectors = false;
 
-  connectors: Connector[] = [
-    { name: 'USB Type C', reversable: true, type: 'rectangle', photoUrl: 'https://www.kabelshop.nl/image/Goobay_USB_C_naar_USB_C_kabel_%7C_0.5_meter_%7C_USB_3.2_100%25_koper_Power_Delivery_100_W_Zwart_49252_K010214083_m1_big.jpg' },
-    { name: 'USB Type B', reversable: false, type: 'rectangle', photoUrl: 'https://www.kantoorspecialist.nl/autoimg/1128438/1024x1024/resize/showimage.jpg' },
-    { name: 'USB Type A', reversable: false, type: 'rectangle', photoUrl: 'https://image.allekabels.nl/image/1513-0/usb-a-kabel-lengte-1.8-meter.jpg' },
-    { name: 'PS/2', reversable: false, type: 'round', photoUrl: 'https://media.startech.com/cms/products/gallery_large/kyc1mf.main.jpg' },
-  ];
+	ngOnInit() {
+		this.http.get<Connector[]>('http://localhost:3000/connectors').subscribe(connectors => {
+			this.connectors = connectors;
+		});
+	}
 
-  addConnector() {
-    this.connectors.push(structuredClone(this.newConnector));
-    this.newConnector = { reversable: false } as Connector;
-  }
+	addConnector() {
+		// this.connectors!.push(structuredClone(this.newConnector));
 
-  handleAutocompleterSelect(connector: Connector) {
-    console.log('yay! werkt!', connector);
-  }
+		this.http
+			.post<Connector>('http://localhost:3000/connectors', this.newConnector)
+			.subscribe(updatedConnector => {
+				this.connectors?.push(updatedConnector);
+			});
 
-  // changeName() {
-  //   setTimeout(() => {
-  //     this.name += ' Youri na timeout';
-  //   }, 2000);
-  // }
+		this.newConnector = { reversable: false } as Connector;
+	}
+
+	handleAutocompleterSelect(connector: Connector) {
+		console.log('yay! werkt!', connector);
+	}
+
+	// changeName() {
+	//   setTimeout(() => {
+	//     this.name += ' Youri na timeout';
+	//   }, 2000);
+	// }
 }
 
 interface Connector {
-  name: string;
-  reversable: boolean;
-  type: string;
-  photoUrl: string;
+	name: string;
+	reversable: boolean;
+	type: string;
+	photoUrl: string;
 }
--- demo/src/app/app.config.ts
+++ demo/src/app/app.config.ts
@@ -1,13 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouterModule } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 // import { NavigateService } from './services/navigate.service';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
+		provideHttpClient(),
+
+		// hip nieuw Angular werkt met provide...()
+		// HttpClientModule,
+		// RouterModule
+
 		// { provide: NavigateService, useClass: NavigateService },
 		// NavigateService,
 	],
